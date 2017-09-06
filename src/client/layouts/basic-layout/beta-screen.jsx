897215137598421bcf98165de6ed88ffde60e4aa
Henri Beck <henribeck.dev@gmail.com>
2017-09-06 17:55:22 +0200

    Added a background image to the beta screen

--- src/client/layouts/basic-layout/beta-screen.jsx
+++ src/client/layouts/basic-layout/beta-screen.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import injectSheet from 'react-jss';
 import PropTypes from 'prop-types';
+import randomItem from 'random-item';
 import {
   Card,
   Button,
 } from 'materialize-react';
 
 import app from '../../app';
+import sixes from '../../../assets/images/background/6v6.jpg';
+import hl from '../../../assets/images/background/9v9.jpg';
+import bball from '../../../assets/images/background/bball.jpg';
+import ultiduo from '../../../assets/images/background/ultiduo.jpg';
 
 /**
  * The screen to display when the client isn't logged in and the site is in beta mode.
@@ -24,7 +29,7 @@ function BetaScreen({ classes }) {
         </Card.Header>
 
         <Card.Content>
-          You will need to login to check if you currently have access to the website.
+          You will need to login to check if you have access to the website.
         </Card.Content>
 
         <Card.Actions className={classes.cardActions}>
@@ -50,6 +55,10 @@ BetaScreen.styles = {
     alignItems: 'center',
     justifyContent: 'center',
     height: '100vh',
+    backgroundImage: `url(${randomItem([sixes, hl, bball, ultiduo])})`,
+    backgroundPosition: 'center center',
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
   },
 
   cardActions: { justifyContent: 'flex-end' },

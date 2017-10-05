abcc94c99de0d5135255f7aa1d3f9e1a4c53e41f
Henri Beck <henribeck.dev@gmail.com>
2017-10-06 01:31:28 +0200

    react

---
+++ src/client/jss.js
@@ -0,0 +1,21 @@
+import { create } from 'jss';
+import { jss as reactJss } from 'react-jss';
+import cache from 'jss-cache';
+import nested from 'jss-nested';
+import compose from 'jss-compose';
+import camelCase from 'jss-camel-case';
+import defaultUnit from 'jss-default-unit';
+import vendorPrefixer from 'jss-vendor-prefixer';
+import propSort from 'jss-props-sort';
+
+const jss = create();
+
+jss.use(cache());
+jss.use(nested());
+jss.use(compose());
+jss.use(camelCase());
+jss.use(defaultUnit());
+jss.use(vendorPrefixer());
+jss.use(propSort());
+
+export default jss;

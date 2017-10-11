5fe25fe69e945378036cdce51e312292ee07ce34
Henri Beck <henribeck.dev@gmail.com>
2017-10-11 18:55:22 +0200

    Fixed pickup-queue filters

--- src/server/services/pickup-queue/filters.js
+++ src/server/services/pickup-queue/filters.js
@@ -4,6 +4,6 @@ export default {
   patched(data, connection) {
     const region = get(connection, 'user.settings.region', 'eu');
 
-    return data.region === region;
+    return data.region === region ? data : false;
   },
 };

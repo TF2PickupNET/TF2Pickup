20779c91102f7d3abddac96d030a8e5e5c2e64ee
Michał Garapich <michal@garapich.pl>
2017-10-12 14:31:45 +0200

    Rules page initial

--- src/client/views/rules/view.jsx
+++ src/client/views/rules/view.jsx
@@ -1,17 +1,38 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import Helmet from 'react-helmet';
+import ReactMarkdown from 'react-markdown';
+
+// TODO Read content from a file
+var content = `
+## TF2Pickup very important rules
+1. First rule
+2. Second
+
+   Properly indented paragraph
+
+   [ETF2L](http://etf2l.org/) account is rqeuired (testing links);
+
+3. Third
+  * Whatever
+  * whatever
+`;
 
 /**
  * The view for the rules page.
  *
  * @returns {JSX} - Returns the view.
  */
-export default function View() {
-  return (
-    <div>
-      <Helmet>
-        <title>Rules</title>
-      </Helmet>
-    </div>
-  );
+export default class View extends PureComponent  {
+
+  render() {
+    return (
+      <div>
+        <Helmet>
+          <title>Rules</title>
+        </Helmet>
+
+        <ReactMarkdown source={content} />
+      </div>
+    );
+  }
 }

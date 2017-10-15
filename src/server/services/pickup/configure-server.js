786a56c7790af43c490e25018b1171c6470bf5ba
PepperKick <abhishrant.m@gmail.com>
2017-10-15 20:36:47 +0530

    added srcds-rcon and server rcon status command

--- src/server/services/pickup/configure-server.js
+++ src/server/services/pickup/configure-server.js
@@ -1,4 +1,24 @@
+import rcon from 'srcds-rcon';
+import debug from 'debug';
+
+const log = debug('TF2Pickup:server');
+
+/**
+ * Start the server.
+ *
+ * @param {Object} props - Props from app.
+ */
 export default async function configureServer(props) {
   const pickup = props.result;
   const server = await props.app.service('servers').get(pickup.serverId);
+  const connection = rcon({
+    address: `${server.ip}:${server.port}`,
+    password: `${server.password}`,
+  });
+
+  await connection.connect();
+
+  const status = await connection.command('status');
+
+  log(`Server: ${server.ip}:${server.port} Status: ${status}`);
 }

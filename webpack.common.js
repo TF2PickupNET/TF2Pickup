405cf9408c0cee6433394fc7ca79e42f701edea5
Henri Beck <henribeck.dev@gmail.com>
2017-10-15 11:20:49 +0200

    Update packages

--- webpack.common.js
+++ webpack.common.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const webpack = require('webpack');
-const dotenv = require('dotenv');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 const { colors } = require('materialize-react');
 const WebpackPwaManifest = require('webpack-pwa-manifest');

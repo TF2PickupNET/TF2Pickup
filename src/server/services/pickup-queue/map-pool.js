3ddd95033c903a5fd87b84a047b6f29e17838e94
Henri Beck <henribeck.dev@gmail.com>
2017-10-16 17:14:01 +0200

    Added map configs

--- src/server/services/pickup-queue/map-pool.js
+++ src/server/services/pickup-queue/map-pool.js
@@ -8,8 +8,21 @@ import {
 
 import mapInfo from '../../../config/maps';
 
-const ultiduoMaps = [];
-const bballMaps = [];
+const ultiduoMaps = [
+  'koth_ultiduo_r_b7',
+  'ultiduo_badlands_b1',
+  'ultiduo_baloo',
+  'ultiduo_gullywash_b2',
+  'ultiduo_nicecicles',
+  'ultiduo_seclusion_b3',
+];
+const bballMaps = [
+  'ctf_ballin_sky',
+  'ctf_bball_alpine_b4',
+  'ctf_bball_comptf',
+  'ctf_bball_snow_b1',
+  'ctf_pro_bball',
+];
 
 const mapPool = {
   eu: {
@@ -60,7 +73,7 @@ const mapPool = {
     ultiduo: ultiduoMaps,
   },
 
-  au: {
+  oc: {
     '6v6': [
       'koth_product_rc8',
       'cp_process_final',
@@ -101,7 +114,7 @@ export function validateMapPool() {
     }
 
     if (mapPoolForGamemode.length < 5) {
-      throw new Error('There should be atleast 5 maps in the map pool');
+      throw new Error(`There should be atleast 5 maps in the map pool ${key}`);
     }
 
     mapPoolForGamemode.forEach((map) => {

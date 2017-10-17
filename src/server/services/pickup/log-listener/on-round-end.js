2b53016f92a51a8add127bf57a5b3fc77cb2c68c
PepperKick <abhishrant.m@gmail.com>
2017-10-17 18:40:29 +0530

    fixed pickup status not patching

--- src/server/services/pickup/log-listener/on-round-end.js
+++ src/server/services/pickup/log-listener/on-round-end.js
@@ -2,8 +2,9 @@ export default {
   line: /World triggered "Game_Over" reason "(.*?)"/,
   // Data[1] - Reason for round end.
   async handler(app, line) {
-    let pickup = await app.service('pickup').find({ query: { logsecret: line.secret } });
+    const pickup = await app.service('pickup').find({ query: { logsecret: line.secret } });
+    const id = parseInt(pickup[0].id, 10);
 
-    app.service('pickup').patch(pickup[0].id, { $set: { status: 'game-finished' } });
+    await app.service('pickup').patch(id, { $set: { status: 'game-finished' } });
   },
 };
--- src/server/services/pickup/log-listener/on-round-start.js
+++ src/server/services/pickup/log-listener/on-round-start.js
@@ -1,9 +1,9 @@
-
 export default {
   line: /World triggered "Round_Start"/,
   async handler(app, line) {
     const pickup = await app.service('pickup').find({ query: { logsecret: line.secret } });
+    const id = parseInt(pickup[0].id, 10);
 
-    app.service('pickup').patch(pickup[0].id, { $set: { status: 'game-is-live' } });
+    await app.service('pickup').patch(id, { $set: { status: 'game-is-live' } });
   },
 };

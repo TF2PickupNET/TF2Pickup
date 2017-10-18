68895f1eaa4153daab73b88e6dd3097b497c3162
Henri Beck <henribeck.dev@gmail.com>
2017-10-18 22:44:41 +0200

    Added setting initial map selection when creating pickups

--- src/server/services/pickup-queue/setup-db.js
+++ src/server/services/pickup-queue/setup-db.js
@@ -1,8 +1,11 @@
 import debug from 'debug';
 
-import regions from '@tf2-pickup/configs/regions';
+import {
+  regions,
+  gamemodes,
+} from '@tf2-pickup/configs';
 
-import gamemodes from '@tf2-pickup/configs/gamemodes';
+import { generateRandomMaps } from './map-pool';
 
 const log = debug('TF2Pickup:pickup-queue:setup-db');
 
@@ -41,6 +44,7 @@ export default function setupDb(service) {
           $set: {
             classes,
             status: 'waiting',
+            maps: gamemodeQueue.maps || generateRandomMaps(region, gamemode),
           },
         });
       } catch (error) {
@@ -53,6 +57,7 @@ export default function setupDb(service) {
             status: 'waiting',
             id: `${region}-${gamemode}`,
             classes,
+            maps: generateRandomMaps(region, gamemode),
           });
 
           return;

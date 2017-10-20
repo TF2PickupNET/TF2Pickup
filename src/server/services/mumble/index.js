235719b91e5229108dfc45c8f69bd3fafa070ff9
PepperKick <abhishrant.m@gmail.com>
2017-10-20 18:30:18 +0530

    converted mumble to service

--- src/server/services/mumble/index.js
+++ src/server/services/mumble/index.js
@@ -1,19 +1,55 @@
+import nodeMumble from 'mumble';
+import config from 'config';
+
+import { getMumbleIP } from '../../../config/index';
+
+import { regions } from '@tf2-pickup/configs/regions';
+
+/**
+ * Mumble service.
+ */
 export default function mumble() {
   const that = this;
+  const connections = {};
 
   that.service('mumble', {
-    async setup() {
-      // Connect to the different regional mumble servers here and authenticate
+    setup() {
+      /**
+      * Adds mumble connection to connections.
+      *
+      * @param {String} regionName - Region name.
+      * @param {Object} connection - Mumble connection.
+      */
+      const setConnection = (regionName, connection) => {
+        connections[regionName] = connection;
+      };
+      const mumbleUsername = config.get('server.mumble.username');
+      const mumblePassword = config.get('server.mumble.password');
+
+      regions.forEach((region) => {
+        const url = getMumbleIP(region.name);
+
+        nodeMumble.connect(url, { }, (error, connection) => {
+          if (error) {
+            throw new Error(error);
+          }
+
+          connection.authenticate(mumbleUsername, mumblePassword);
+          connection.on('initialized', setConnection);
+        });
+      });
     },
 
-    // Create a mumble channel here
-    async create({ name }) {
+    create(region, name) {
+      const channel = connections[region].channelByName('Pickups');
 
+      channel.addSubChannel(name);
     },
 
-    // Delete a mumble channel here
-    async delete({ name }) {
+    delete(region, name) {
+      const channel = connections[region].channelByName(name);
 
+      channel.remove(name);
     },
   });
 }
--- src/server/services/pickup/hooks.js
+++ src/server/services/pickup/hooks.js
@@ -1,19 +1,20 @@
 import configureServer from './configure-server';
-import createMumbleChannel from './create-mumble-channel';
-import removeMumbleChannel from './remove-mumble-channel';
 
 export default {
   after: {
     create(props) {
-      createMumbleChannel(props);
       configureServer(props);
+
+      props.app.service('mumble').create(props.status.region, props.status.id);
 
       return props;
     },
 
     patch(props) {
-      if (props.result.status === 'game-finished' || props.result.status === 'server-configuration-error') {
-        removeMumbleChannel(props);
+      const serverStatus = [ 'game-finished', 'server-configuration-error' ];
+
+      if (serverStatus.includes(props.result.status)) {
+        props.app.service('mumble').create(props.status.region, props.status.mumbleChannel);
       }
 
       return props;

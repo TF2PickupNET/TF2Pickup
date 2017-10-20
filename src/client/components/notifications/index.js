c7d387df667237c1109ad51e3564571e6f42f0a6
Henri Beck <henribeck.dev@gmail.com>
2017-10-20 10:22:41 +0200

    Added displaying notifications from the server

--- src/client/components/notifications/index.js
+++ src/client/components/notifications/index.js
@@ -1,15 +1,1 @@
-import { connect } from 'react-redux';
-
-import { removeNotification } from '../../redux/notifications/actions';
-
-import Notifications from './notifications';
-
-export default connect((state) => {
-  return { snackbars: state.notifications };
-}, (dispatch) => {
-  return {
-    removeNotification(id) {
-      return dispatch(removeNotification(id));
-    },
-  };
-})(Notifications);
+export default from './notifications';

804d6975c0cbcd10a647114c62d82b36fa7bf341
PepperKick <abhishrant.m@gmail.com>
2017-10-20 16:33:59 +0530

    added remove mumble channel code

--- src/server/services/pickup/hooks.js
+++ src/server/services/pickup/hooks.js
@@ -1,11 +1,20 @@
 import configureServer from './configure-server';
 import createMumbleChannel from './create-mumble-channel';
+import removeMumbleChannel from './remove-mumble-channel';
 
 export default {
   after: {
     create(props) {
       createMumbleChannel(props);
       configureServer(props);
+
+      return props;
+    },
+
+    patch(props) {
+      if (props.result.status === 'game-finished' || props.result.status === 'server-configuration-error') {
+        removeMumbleChannel(props);
+      }
 
       return props;
     },
--- src/server/services/pickup/remove-mumble-channel.js
+++ src/server/services/pickup/remove-mumble-channel.js
@@ -2,14 +2,13 @@ import mumble from 'mumble';
 import config from 'config';
 
 /**
- * Create a channel in mumble.
+ * Remaov a channel in mumble.
  *
  * @param {Object} props - Props from app.
  */
-export default function createMumbleChannel(props) {
+export default function removeMumbleChannel(props) {
   const pickup = props.result;
-  const pickupService = props.app.service('pickup');
-  const channelName = `${pickup.id}`;
+  const channelName = pickup.mumbleChannel;
 
   mumble.connect(config.get(`server.mumble.${pickup.region}`), { }, (error, conn) => {
     if (error) {
@@ -18,15 +17,13 @@ export default function createMumbleChannel(props) {
 
     conn.authenticate(config.get('server.mumble.username'), config.get('server.mumble.password'));
     conn.on('initialized', () => {
-      const channel = conn.channelByName('Lobbies');
+      const channel = conn.channelByName(channelName);
 
       if (!channel) {
-        throw new Error('Default channel "Lobbies" does not exist');
+        throw new Error('Unable to find mumble channel for pickup', pickup.id);
       }
 
-      channel.addSubChannel(channelName);
-
-      pickupService.get(pickup.id).patch({ mumbleChannel: channelName });
+      channel.remove(channelName);
     });
   });
 }

05e277247c0277f770ac04b4bab725ffa6bc5634
Henri Beck <henribeck.dev@gmail.com>
2017-10-20 09:25:35 +0200

    Removed unnecessary log line

--- src/server/services/pickup-queue/statuses/ready-up/serveme-strategy.js
+++ src/server/services/pickup-queue/statuses/ready-up/serveme-strategy.js
@@ -53,7 +53,6 @@ export default async function servemeStrategy(url, key) {
       stvPassword: STV_PASSWORD,
       reservationId: reservation.data.reservation.id,
     },
-    serverId: reservation.data.reservation.id,
     logSecret: reservation.data.reservation.logsecret.toString(),
   };
 }

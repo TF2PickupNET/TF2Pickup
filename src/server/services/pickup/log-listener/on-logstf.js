2c26b7b4752722a0d5065f2c599512e2cab3a405
Henri Beck <henribeck.dev@gmail.com>
2017-12-20 18:50:13 +0100

    Added calling the calculate-elo endpoint when the game finishes

--- src/server/services/pickup/log-listener/on-logstf.js
+++ src/server/services/pickup/log-listener/on-logstf.js
@@ -1,3 +1,8 @@
+import axios from 'axios';
+import debug from 'debug';
+
+const log = debug('TF2Pickup:pickup:log-listener:logstf');
+
 export default {
   line: /The log is available here: (http|https):\/\/(.*?)\/(.*?) \./,
 
@@ -7,6 +12,20 @@ export default {
   async handler(app, line) {
     const pickup = await app.service('pickup').find({ query: { logsecret: line.secret } });
 
-    await app.service('pickup').patch(pickup.id, { $set: { logsTFID: line.data[3] } });
+    await app.service('pickup').patch(pickup[0].id, { $set: { logsTFID: line.data[3] } });
+
+    if (app.get('env') === 'prod') {
+      try {
+        await axios.post('localhost:9000/calculate-elo', { pickupId: pickup[0].id });
+      } catch (error) {
+        log('Error while calculating the elo for pickup', pickup.id, error);
+
+        await app.service('discord-message').create({
+          channel: 'errors',
+          message: `The request for calculating the elo after the pickup ${pickup[0].id}
+           returned with an error`,
+        });
+      }
+    }
   },
 };
--- src/server/services/pickup/log-listener/on-round-start.js
+++ src/server/services/pickup/log-listener/on-round-start.js
@@ -4,7 +4,7 @@ export default {
     const pickup = await app.service('pickup').find({ query: { logsecret: line.secret } });
 
     if (pickup.status !== 'game-is-live') {
-      await app.service('pickup').patch(pickup.id, { $set: { status: 'game-is-live' } });
+      await app.service('pickup').patch(pickup[0].id, { $set: { status: 'game-is-live' } });
     }
   },
 };

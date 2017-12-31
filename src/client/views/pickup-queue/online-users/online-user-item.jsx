164cd299bd76e551d87961535fba64f714cd7278
Henri Beck <henribeck.dev@gmail.com>
2017-12-31 11:13:05 +0100

    Added client code for alerting a user

--- src/client/views/pickup-queue/online-users/online-user-item.jsx
+++ src/client/views/pickup-queue/online-users/online-user-item.jsx
@@ -1,65 +1,94 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import { connect } from 'react-redux';
 import injectSheet from 'react-jss';
 import PropTypes from 'prop-types';
+import { IconButton } from 'materialize-react';
 
 import UserItem from '../../../components/user-item/index';
+import hasPermission from '../../../../utils/has-permission';
+import app from '../../../app';
 
 /**
- * The list item for a user in the online users list.
+ * The item for a user in the online users item.
  *
- * @param {Object} props - The props for the component.
- * @returns {JSX} - Returns the JSX.
+ * @class
  */
-function OnlineUserItem(props) {
-  return (
-    <span className={props.classes.item}>
-      <img
-        alt="avatar"
-        src={props.user.avatar}
-        className={props.classes.avatar}
-      />
+class OnlineUserItem extends PureComponent {
+  static propTypes = {
+    classes: PropTypes.shape({
+      item: PropTypes.string.isRequired,
+      avatar: PropTypes.string.isRequired,
+      iconContainer: PropTypes.string.isRequired,
+    }).isRequired,
+    user: PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      avatar: PropTypes.string.isRequired,
+    }).isRequired,
+    canAlert: PropTypes.bool.isRequired,
+  };
 
-      <UserItem
-        user={props.user}
-        className={props.classes.userItem}
-      />
-    </span>
-  );
+  static styles = {
+    avatar: {
+      borderRadius: '50%',
+      height: '100%',
+      width: '100%',
+    },
+
+    item: {
+      display: 'grid',
+      gridTemplateColumns: '32px 1fr 32px',
+      alignItems: 'center',
+      gridGap: '8px',
+      padding: '4px 12px',
+    },
+
+    iconContainer: {
+      height: 32,
+      width: 32,
+      padding: 8,
+      margin: 0,
+
+      '& .icon::before': { fontSize: '16px !important' },
+    },
+  };
+
+  /**
+   * Emit the socket.io event for alerting a user.
+   */
+  handleAlertPress = () => {
+    app.io.emit('user.alert', { userId: this.props.user.id });
+  };
+
+  render() {
+    return (
+      <span className={this.props.classes.item}>
+        <img
+          alt="avatar"
+          src={this.props.user.avatar}
+          className={this.props.classes.avatar}
+        />
+
+        <UserItem user={this.props.user} />
+
+        {this.props.canAlert ? (
+          <IconButton
+            icon="account-alert"
+            className={this.props.classes.iconContainer}
+            onPress={this.handleAlertPress}
+          />
+        ) : null}
+      </span>
+    );
+  }
 }
-
-OnlineUserItem.propTypes = {
-  classes: PropTypes.shape({
-    item: PropTypes.string.isRequired,
-    avatar: PropTypes.string.isRequired,
-    userItem: PropTypes.string.isRequired,
-  }).isRequired,
-  user: PropTypes.shape({ avatar: PropTypes.string.isRequired }).isRequired,
-};
-
-OnlineUserItem.styles = {
-  avatar: {
-    borderRadius: '50%',
-    height: 32,
-    width: 32,
-  },
-
-  item: {
-    display: 'flex',
-    alignItems: 'center',
-    height: 40,
-    padding: '4px 12px',
-    boxSizing: 'border-box',
-  },
-
-  userItem: {
-    flex: 1,
-    padding: '0 8px',
-  },
-};
 
 export default connect(
   (state, props) => {
-    return { user: state.onlineUsers[props.id] };
+    const user = state.onlineUsers[props.id];
+
+    return {
+      user,
+      canAlert: hasPermission('user.alert', state.user, user),
+    };
   },
 )(injectSheet(OnlineUserItem.styles)(OnlineUserItem));

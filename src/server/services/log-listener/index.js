70a3985c9f273368a147ec40e61e1ddf056bdbae
Henri Beck <henribeck.dev@gmail.com>
2018-01-13 12:01:56 +0100

    Update log listener service

--- src/server/services/log-listener/index.js
+++ src/server/services/log-listener/index.js
@@ -8,76 +8,54 @@ import events from './events';
 
 const log = debug('TF2Pickup:log-listener');
 
-/**
- * The log listener service.
- *
- * @class
- */
-class LogListenerService {
-  /**
-   * Process a line from the server.
-   *
-   * @param {String} rawLine - The raw line as a string.
-   * @returns {Object} - Returns the data for the line.
-   */
-  static processLine(rawLine) {
-    const regex = /S(.*?)L (.*?) - (.*?): (.*)/g;
-    const matches = regex.exec(rawLine);
+function processLine(rawLine) {
+  const regex = /S(.*?)L (.*?) - (.*?): (.*)/g;
+  const matches = regex.exec(rawLine);
 
-    const line = {
-      secret: matches[1],
-      date: matches[2],
-      time: matches[3],
-      data: matches[4],
-      raw: rawLine,
-    };
+  const line = {
+    secret: matches[1],
+    date: matches[2],
+    time: matches[3],
+    data: matches[4],
+    raw: rawLine,
+  };
 
-    if (!line.secret || !line.date || !line.time || !line.data) {
-      throw new Error('Unknown line');
-    }
-
-    return line;
+  if (!line.secret || !line.date || !line.time || !line.data) {
+    throw new Error('Unknown line');
   }
 
-  /**
-   * The setup method for the service.
-   * This will start the UDP listener.
-   *
-   * @param {Object} app - The feathers app object.
-   */
-  setup(app) {
-    this.app = app;
+  return line;
+}
 
+async function onDataHandler(app, data) {
+  const line = processLine(data.toString('utf8'));
+  const handler = find(event => event.line.test(line.data))(events);
+
+  if (handler) {
+    const [pickup] = await app.service('pickup').find({ query: { logSecret: line.secret } });
+
+    if (pickup) {
+      await handler.handler(app, pickup, {
+        ...line,
+        data: handler.line.exec(line.data),
+      });
+    }
+  }
+}
+
+const LogListenerService = {
+  setup(app) {
     const connection = udp({
       address: config.get('server.ip'),
       bindingPort: config.get('server.log_listener_port'),
       reuseAddr: true,
     });
 
-    connection.on('data', this.onData);
-  }
-
-  /**
-   * When the UDP connection receives data, check if we want to handle that lines.
-   *
-   * @param {Object} data - The data from the server.
-   */
-  onData = async (data) => {
-    const line = LogListenerService.processLine(data.toString('utf8'));
-    const handler = find(event => event.line.test(line.data))(events);
-
-    if (handler) {
-      const [pickup] = await this.app.service('pickup').find({ query: { logsecret: line.secret } });
-
-      if (pickup) {
-        await handler.handler(this.app, pickup, {
-          ...line,
-          data: handler.line.exec(line.data),
-        });
-      }
-    }
-  };
-}
+    connection.on('data', (data) => {
+      onDataHandler(app, data);
+    });
+  },
+};
 
 /**
  * Setup the log listener service.
@@ -87,5 +65,5 @@ export default function logListener() {
 
   log('Setting up log listener service');
 
-  that.service('log-listener', new LogListenerService());
+  that.service('log-listener', LogListenerService);
 }

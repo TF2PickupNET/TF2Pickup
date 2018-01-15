af21a6dd8e26c7aed971c05c313d774c7b892374
Henri Beck <henribeck.dev@gmail.com>
2018-01-15 23:45:57 +0100

    Added socket method for deleting a chat message

--- src/server/services/chat/socket-methods.js
+++ src/server/services/chat/socket-methods.js
@@ -1,4 +1,10 @@
+import debug from 'debug';
+
+import hasPermission from '../../../utils/has-permission';
+
 import { markdownFormatter } from './message-formatters';
+
+const log = debug('TF2Pickup:chat:socket-methods');
 
 /**
  * Setup the socket methods for the chat.
@@ -11,10 +17,32 @@ export default function socketMethods(app, socket) {
     message,
     chat,
   }) => {
-    await app.service('chat').create({
-      userId: socket.feathers.user.id,
-      message: markdownFormatter(message),
-      chat,
-    });
+    if (socket.feathers.user) {
+      log('Creating new chat message', message, chat, socket.feathers.user.id);
+
+      await app.service('chat').create({
+        userId: socket.feathers.user.id,
+        message: markdownFormatter(message),
+        chat,
+      });
+    }
+  });
+
+  socket.on('chat.delete-message', async ({ messageId }) => {
+    try {
+      const message = await app.service('chat').get(messageId);
+      const user = await app.service('users').get(message.userId);
+
+      if (hasPermission('chat.delete', socket.feathers.user, user)) {
+        await app.service('chat').patch(messageId, {
+          $set: {
+            removed: true,
+            removedBy: socket.feathers.user.id,
+          },
+        });
+      }
+    } catch (error) {
+      log('Error while trying to delete message', messageId, error);
+    }
   });
 }

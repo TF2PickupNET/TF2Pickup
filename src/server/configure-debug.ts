74d8bc0132d0bbd389a3f44c7bb3985f33771e47
Henri Beck <henribeck.dev@gmail.com>
2019-02-10 12:26:46 +0100

    Fix logger
    Signed-off-by: Henri Beck <henribeck.dev@gmail.com>

--- src/server/configure-debug.ts
+++ src/server/configure-debug.ts
@@ -4,6 +4,7 @@ import stripAnsi from 'strip-ansi';
 import PrettyError from 'pretty-error';
 import mongoose from 'mongoose';
 import Log from '@typings/Log';
+import { isString } from '@utils/string';
 
 const logs: Log[] = [];
 const pe = new PrettyError();
@@ -11,7 +12,10 @@ const pe = new PrettyError();
 pe.skipNodeFiles();
 pe.skipPackage('bson');
 
-type Logger = <ExtraData>(message: string, data: Data<ExtraData>) => void;
+interface Logger {
+  <ExtraData>(message: string, timestamp: string, data: Data<ExtraData>): void,
+  <ExtraData>(message: string, data: Data<ExtraData>): void,
+}
 
 // eslint-disable-next-line unicorn/no-unsafe-regex
 const LINE_REGEX = /^(TF2Pickup(?::[\w-]+)+) (.+) (\+\d+ms)$/;
@@ -34,14 +38,14 @@ function parseLine(message: string, data: Data<{}>): Log | null {
   };
 }
 
-function logDebug(message: string, data: Data<{}> = {}) {
+function logDebug(message: string, timestamp: string, data: Data<{}> = {}) {
   const {
     error,
     ...other
   } = data;
 
   // eslint-disable-next-line no-console
-  console.info(message, other);
+  console.info(message, timestamp, other);
 
   if (error) {
     // eslint-disable-next-line no-console
@@ -50,16 +54,18 @@ function logDebug(message: string, data: Data<{}> = {}) {
 }
 
 function createLogger(callback: (log: Log) => void): Logger {
-  return (message, data) => {
-    logDebug(message, data);
+  return (message: string, timestamp: string | Data<{}>, data?: Data<{}>) => {
+    logDebug(
+      message,
+      isString(timestamp) ? timestamp : '',
+      isString(timestamp) ? data : timestamp,
+    );
 
     const log = parseLine(stripAnsi(message), data);
 
     if (log !== null) {
-      return callback(log);
+      callback(log);
     }
-
-    return null;
   };
 }
 

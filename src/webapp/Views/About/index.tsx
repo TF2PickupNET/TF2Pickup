16082fc3e11fd4c9ea3f6c57153be08240ded081
Henri Beck <henribeck.dev@gmail.com>
2019-02-16 11:58:13 +0100

    Fix one more type issue
    Signed-off-by: Henri Beck <henribeck.dev@gmail.com>

--- src/webapp/Views/About/index.tsx
+++ src/webapp/Views/About/index.tsx
@@ -1,11 +1,28 @@
 import React from 'react';
+import withStyles, { WithStyles } from 'react-jss';
+import { Column, Row } from '@webapp/components/Grid';
 
-function About() {
+const styles = {
+  container: { padding: 32 },
+};
+
+interface Props extends WithStyles<typeof styles> {
+  path: string,
+}
+
+function About(props: Props) {
   return (
-    <div>
-      About
-    </div>
+    <Row
+      justify="center"
+      className={props.classes.container}
+    >
+      <Column col={16}>
+        <h3>
+          About TF2Pickup
+        </h3>
+      </Column>
+    </Row>
   );
 }
 
-export default About;
+export default withStyles(styles)(About);
